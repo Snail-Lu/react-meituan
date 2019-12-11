58602ffb095bb3c5095b70a8877d1b429dd4314b
路明 <snaillu@yeah.net>
2019-12-11 17:39:56 +0800

    project init

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+    mode: "development",
+    // 入口文件
+    entry: {
+        app: './src/index.js'
+    },
+    // 输出到dist文件夹, 文件名字为bundle.js
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname,'./dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                // use: ['babel-loader','eslint-loader'],  //必要的时候再用
+                use: ['babel-loader'],                   
+                exclude: /node_modules/
+            }
+        ]
+    },
+    devServer: {
+        port: 3000,
+        contentBase: './dist'
+    }
+}
