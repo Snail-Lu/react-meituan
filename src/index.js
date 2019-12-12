745408da09ed590b06f33163419cfe7642d648d5
路明 <snaillu@yeah.net>
2019-12-12 18:31:56 +0800

    首页html及css编写

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom'
 import App from './App';
+import '../src/style/base.scss';
 
 render(
     <App />,
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -5,7 +5,37 @@ export default class Home extends Component {
     render() {
         return (
             <div className="home">
-                欢迎使用美团
+                {/* 顶部导航栏 */}
+                <header className="navbar">
+                    <div className="nav-wrap-left">
+                        <span>上海</span>
+                        <img src="https://p0.meituan.net/travelcube/45c79a92755b54adc9dc1c4682b123b3201.png" alt=""/>
+                    </div>
+                    <div className="box-search">
+                        <a href="#">
+                            <img className="icn_search" src="https://p0.meituan.net/travelcube/99c29829cf1b85d5cdbc76a1bd0b7329814.png" alt=""/>
+                            <span className="txt_search">请输入商家名，品类或商圈...</span>
+                        </a>
+                    </div>
+                    <div className="nav-wrap-right">
+                        <img className="icn_mine" src="https://p0.meituan.net/travelcube/641521461179df7cfb88738dd1ea11ec1031.png" alt=""/>
+                    </div>
+                </header>
+                {/* App下载导航栏 */}
+                <div className="banner-download">
+                    <div className="banner-item">
+                        <img className="img-box" src="https://p1.meituan.net/travelcube/7264ce9c25de2e464e3acd996fe8ad362803.png" alt=""/>
+                        <div className="logo-theme">
+                            <p className="logo-tit">省钱利器 购物超划算</p>
+                            <p className="logo-desc">吃喝玩乐尽在美团</p>
+                        </div>
+                    </div>
+                    <div className="to-app-btn">去APP</div>
+                </div>
+                {/* 菜单栏 */}
+                <div className="listbar">
+                    {}
+                </div>
             </div>
         )
     }
