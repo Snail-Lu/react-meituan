31e93a4a39bfa1ef3ade3bab19ce7bc8efa3c081
路明 <snaillu@yeah.net>
2019-12-27 19:13:13 +0800

    路由调整

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Home from './pages/home/home';
 import Foods from './pages/foods/foods';
 import Movie from './pages/movies/movies';
-import My from './pages/movies/my/my';
 import { BrowserRouter,Switch,Route,IndexRoute } from 'react-router-dom';
 
 export default class App extends Component {
@@ -12,7 +11,6 @@ export default class App extends Component {
                 <Route path="/" exact component={Home} />
                 <Route path="/foods" component={Foods} />
                 <Route path="/movies" component={Movie} />
-                <Route path="/my" component={My} />
             </BrowserRouter>
         )
     }
